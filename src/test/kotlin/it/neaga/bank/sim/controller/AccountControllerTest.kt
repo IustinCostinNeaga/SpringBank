913d564805aa8078a1d7663bb57f242f0721f95e
Iustin Costin Neaga <iustincostinneaga@gmail.com>
2026-04-20 21:22:55 +0200

    refactor: remove worthless test

--- src/test/kotlin/it/neaga/bank/sim/controller/AccountControllerTest.kt
+++ src/test/kotlin/it/neaga/bank/sim/controller/AccountControllerTest.kt
@@ -6,7 +6,6 @@ import it.neaga.bank.sim.factories.AccountFactories.newAccountRequest
 import it.neaga.bank.sim.factories.AccountFactories.newAccountResponse
 import it.neaga.bank.sim.model.Account
 import it.neaga.bank.sim.service.AccountService
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
@@ -22,16 +21,10 @@ import org.springframework.test.web.servlet.client.expectBody
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureRestTestClient
-class AccountControllerTest(@Autowired var accountController: AccountController, @Autowired private val webClient: RestTestClient) {
+class AccountControllerTest(@Autowired private val webClient: RestTestClient) {
 
     @MockitoBean
     lateinit var accountService: AccountService
-
-    @Test
-    @DisplayName("should load AccountController")
-    fun loadAccountController() {
-        assertThat(accountController).isNotNull
-    }
 
     @Test
     @DisplayName("should create a new account correctlly")
