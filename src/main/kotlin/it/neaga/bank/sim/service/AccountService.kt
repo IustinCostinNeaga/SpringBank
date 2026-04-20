098087707903a26ac3dfa3c98a98f62f9f438f01
Iustin Costin Neaga <iustincostinneaga@gmail.com>
2026-04-20 20:11:14 +0200

    feat & test: create support function for IBAN generation and created AccountServiceTest

--- src/main/kotlin/it/neaga/bank/sim/service/AccountService.kt
+++ src/main/kotlin/it/neaga/bank/sim/service/AccountService.kt
@@ -2,12 +2,33 @@
 
 import it.neaga.bank.sim.dto.request.NewAccountRequest
 import it.neaga.bank.sim.dto.response.NewAccountResponse
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import kotlin.random.Random
+
+@Component
+class IbanGenerator {
+    fun generateItIban(): String {
+
+        val nationCode = "IT"
+        val cinEU = (0..99).random().toString().padStart(2, '0')
+        val cin = randomUpperLetter().toString()
+        val abi = (0..9999999).random().toString().padStart(5, '0')
+        val cab = (0..9999999).random().toString().padStart(5, '0')
+        val ccNumber = (0..12).joinToString("") { randomAlphaNumeric().toString() }
+
+        return nationCode + cinEU + cin + abi + cab + ccNumber
+    }
+
+    private fun randomUpperLetter(): Char = ('A'..'Z').random()
+
+    private fun randomAlphaNumeric(): Char = if ((0..1).random() == 0) randomUpperLetter() else ('0'..'9').random()
+}
 
 @Service
-class AccountService {
+class AccountService(private val ibanGenerator: IbanGenerator) {
 
-    fun createNewAccount(newAccount: NewAccountRequest): NewAccountResponse{
+    fun createNewAccount(newAccount: NewAccountRequest): NewAccountResponse {
         TODO()
     }
 
---
+++ src/test/kotlin/it/neaga/bank/sim/service/AccountServiceTest.kt
@@ -0,0 +1,27 @@
+﻿package it.neaga.bank.sim.service
+
+import it.neaga.bank.sim.dto.request.NewAccountRequest
+import it.neaga.bank.sim.factories.AccountFactories.newAccountRequest
+import it.neaga.bank.sim.factories.AccountFactories.newAccountResponse
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+
+@SpringBootTest
+class AccountServiceTest(@Autowired val accountService: AccountService) {
+
+    @Test
+    @DisplayName("should create a new account")
+    fun accountCreationTest(){
+
+        val result = accountService.createNewAccount(newAccountRequest())
+        assertThat(result).isEqualTo(newAccountResponse())
+
+    }
+
+
+}
