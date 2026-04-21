bacd42fd76bc70350d26572d7bcba96122365a1b
Iustin Costin Neaga <iustincostinneaga@gmail.com>
2026-04-21 20:25:58 +0200

    test: add error handling test for client api call

--- src/test/kotlin/it/neaga/bank/sim/client/CurrencyExchangeClientTest.kt
+++ src/test/kotlin/it/neaga/bank/sim/client/CurrencyExchangeClientTest.kt
@@ -5,17 +5,24 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.restclient.test.autoconfigure.RestClientTest
 import org.springframework.boot.test.context.PropertyMapping
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.crossstore.ChangeSetPersister
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.client.MockRestServiceServer
 import org.springframework.test.web.client.match.MockRestRequestMatchers.method
 import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withResourceNotFound
+import org.springframework.test.web.client.response.MockRestResponseCreators.withStatus
 import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
+import org.springframework.web.client.HttpClientErrorException
+import kotlin.test.assertFailsWith
 
 @RestClientTest(CurrencyExchangeClient::class)
 @TestPropertySource(properties = ["external-api.exchange.base-url=http://mock-url"])
@@ -34,6 +41,19 @@ class CurrencyExchangeClientTest(@Autowired var currencyExchangeClient: Currency
         val rate = currencyExchangeClient.getRate(Currency.EUR, Currency.USD)
 
         assertThat(rate).isEqualTo(1.1769)
+    }
+    @Test
+    @DisplayName("should throw an exception if currency is not correct")
+    fun handleExchangeRateException(){
+        server.expect(requestTo("http://mock-url/rate/EUR/USD"))
+            .andExpect(method(HttpMethod.GET))
+            .andRespond(withStatus(HttpStatus.NOT_FOUND))
+
+        val exception = assertThrows<HttpClientErrorException> {
+            currencyExchangeClient.getRate(Currency.EUR, Currency.USD)
+        }
+
+        assertThat(exception.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
     }
 
     val jsonResponse = """
